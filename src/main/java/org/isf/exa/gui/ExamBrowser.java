6300a0f1b4edf3a8dead5efa4d054fb106bda6da
dbmalkovsky <david@malkovsky.org>
2020-09-07 10:50:59 -0400

    OP-256 add license plugin and add headers

--- src/main/java/org/isf/exa/gui/ExamBrowser.java
+++ src/main/java/org/isf/exa/gui/ExamBrowser.java
@@ -1,3 +1,26 @@
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2020 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.isf.exa.gui;
+
 /*------------------------------------------
  * ExamBrowser - list all exams. let the user select an exam to edit
  * -----------------------------------------
@@ -7,8 +30,6 @@
  * 			         - version is now 1.0 
  * 10/11/2006 - ross - corretto eliminazione esame, prima non si cancellava mai
  *------------------------------------------*/
-
-package org.isf.exa.gui;
 
 import java.awt.AWTEvent;
 import java.awt.BorderLayout;
--- startup.cmd
+++ startup.cmd
@@ -1,3 +1,25 @@
+@REM
+@REM Open Hospital (www.open-hospital.org)
+@REM Copyright © 2006-2020 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+@REM
+@REM Open Hospital is a free and open source software for healthcare data management.
+@REM
+@REM This program is free software: you can redistribute it and/or modify
+@REM it under the terms of the GNU General Public License as published by
+@REM the Free Software Foundation, either version 3 of the License, or
+@REM (at your option) any later version.
+@REM
+@REM https://www.gnu.org/licenses/gpl-3.0-standalone.html
+@REM
+@REM This program is distributed in the hope that it will be useful,
+@REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+@REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@REM GNU General Public License for more details.
+@REM
+@REM You should have received a copy of the GNU General Public License
+@REM along with this program. If not, see <http://www.gnu.org/licenses/>.
+@REM
+
 @echo off
 set OH_HOME=%~dps0
 REM if java is not in the system path set JAVA_HOME variable
