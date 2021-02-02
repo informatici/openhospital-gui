5a1fac090582f3e34c0b8daff2c416f8af2fc612
dbmalkovsky <david@malkovsky.org>
2021-02-02 08:59:13 -0500

    OP-428 remove unused import

--- src/main/java/org/isf/lab/gui/LabNew.java
+++ src/main/java/org/isf/lab/gui/LabNew.java
@@ -41,7 +41,6 @@ import java.util.logging.Logger;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
