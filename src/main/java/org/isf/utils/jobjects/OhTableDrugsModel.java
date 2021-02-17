6e3e16ec88f3e20727854533e02635a596d655ad
David B Malkovsky <david@malkovsky.org>
2021-02-17 15:44:39 -0500

    OP-442 remove unread fields (#420)
    * OP-442 remove unread fields
    * OP-442 use the correct Locale
    * OP-442 remove TODO in empty methods

--- src/main/java/org/isf/utils/jobjects/OhTableDrugsModel.java
+++ src/main/java/org/isf/utils/jobjects/OhTableDrugsModel.java
@@ -30,33 +30,28 @@ import java.util.Locale;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
+import org.isf.generaldata.GeneralData;
 import org.isf.generaldata.MessageBundle;
 import org.isf.medicals.model.Medical;
 import org.isf.medicalstockward.model.MovementWard;
-import org.isf.operation.manager.OperationBrowserManager;
 
 public class OhTableDrugsModel <T> implements TableModel {
 
-	List<T> dataList;	
 	List<T> filteredList;
-	OperationBrowserManager manageop = new OperationBrowserManager();
-	
+
 	public  OhTableDrugsModel(List<T> dataList) {
-		this.dataList = dataList;
 		this.filteredList = new ArrayList<T>();
-		
-		if (dataList!=null){
+
+		if (dataList != null) {
 			for (Iterator<T> iterator = dataList.iterator(); iterator.hasNext();) {
 				T t = (T) iterator.next();
-				this.filteredList.add(t);			
+				this.filteredList.add(t);
 			}
 		}
 	}
-	
+
 	@Override
 	public void addTableModelListener(TableModelListener l) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -71,7 +66,7 @@ public class OhTableDrugsModel <T> implements TableModel {
 
 	@Override
 	public String getColumnName(int columnIndex) {
-		String columnLabel="";
+		String columnLabel = "";
 		switch (columnIndex) {
 		case 0:
 			columnLabel= MessageBundle.getMessage("angal.medicalstockward.patient.date");
@@ -88,7 +83,7 @@ public class OhTableDrugsModel <T> implements TableModel {
 		case 3:
 			columnLabel= MessageBundle.getMessage("angal.medicalstockward.patient.units");
 			//columnLabel= "Unite Trans";
-			break;	
+			break;
 		default:
 			break;
 		}
@@ -97,7 +92,7 @@ public class OhTableDrugsModel <T> implements TableModel {
 
 	@Override
 	public int getRowCount() {
-		if (this.filteredList==null){
+		if (this.filteredList == null) {
 			return 0;
 		}
 		return this.filteredList.size();
@@ -105,64 +100,58 @@ public class OhTableDrugsModel <T> implements TableModel {
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		String value="";
-		if (rowIndex >=0 && rowIndex < this.filteredList.size()){
+		String value = "";
+		if (rowIndex >= 0 && rowIndex < this.filteredList.size()) {
 			T obj=this.filteredList.get(rowIndex);
-			if (obj instanceof MovementWard){
-				MovementWard DrugObj=(MovementWard)obj;
+			if (obj instanceof MovementWard) {
+				MovementWard drugObj=(MovementWard)obj;
 				switch (columnIndex) {
 				case 0:
 					String dt = "";
 					try {
-						final DateFormat currentDateFormat = DateFormat.getDateInstance(DateFormat.SHORT, Locale.FRENCH);
-						dt = currentDateFormat.format(DrugObj.getDate().getTime());
+						final DateFormat currentDateFormat = DateFormat.getDateInstance(DateFormat.SHORT, new Locale(GeneralData.LANGUAGE));
+						dt = currentDateFormat.format(drugObj.getDate().getTime());
 						value = dt;
 					}
-					catch (Exception ex){
-						value=DrugObj.getDate().getTime().toString();
+					catch (Exception ex) {
+						value = drugObj.getDate().getTime().toString();
 					}
-					
+
 					break;
 				case 1:
-										Medical drugsname = null;
+					Medical drugsname = null;
 					//System.out.println("Looking operation whose code is " + opdObj.getOperation().getCode());
-					   drugsname = DrugObj.getMedical();
+					drugsname = drugObj.getMedical();
 					if (drugsname != null)
 						value = drugsname.getDescription();
 					else
 						value = "";
 					break;
 				case 2:
-					value=String.valueOf(DrugObj.getQuantity());
+					value=String.valueOf(drugObj.getQuantity());
 					break;
 				case 3:
-					value=DrugObj.getUnits();
-					break;	
+					value=drugObj.getUnits();
+					break;
 				default:
 					break;
 				}
-			}			
+			}
 		}
 		return value;
 	}
 
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void removeTableModelListener(TableModelListener l) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
