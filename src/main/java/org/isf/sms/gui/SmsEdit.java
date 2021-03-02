bde5b35fe372480b3c2fb5ea936a259201247d3f
dbmalkovsky <david@malkovsky.org>
2021-03-02 18:53:22 -0500

    OP-454 update for method name in Core

--- src/main/java/org/isf/sms/gui/SmsEdit.java
+++ src/main/java/org/isf/sms/gui/SmsEdit.java
@@ -113,7 +113,7 @@ public class SmsEdit extends JDialog implements SelectionListener {
 	}
 	
 	private void initialize() {
-		MAX_LENGTH = smsManager.getMAX_LENGHT();
+		MAX_LENGTH = smsManager.getMaxLength();
 	}
 
 	private void initComponents() {
