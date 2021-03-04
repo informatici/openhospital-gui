90254d6878bc3e342b7c1ad4f97a571e55b1b137
dbmalkovsky <david@malkovsky.org>
2021-03-04 14:22:17 -0500

    Chore: use contains() instead of indexOf()

--- src/main/java/org/isf/dicom/gui/FileDicomFilter.java
+++ src/main/java/org/isf/dicom/gui/FileDicomFilter.java
@@ -31,28 +31,23 @@ import javax.swing.filechooser.FileView;
  * @author Pietro Castellucci
  * @version 1.0.0
  */
-public class FileDicomFilter
-        extends javax.swing.filechooser.FileFilter
-{
+public class FileDicomFilter extends javax.swing.filechooser.FileFilter {
 
-    /**
-     * Whether the given file is accepted by this filter.
-     */
-    public boolean accept(File f)
-    {
+	/**
+	 * Whether the given file is accepted by this filter.
+	 */
+	public boolean accept(File f) {
+		return (f.getName().toLowerCase().endsWith(".dcm")
+				|| f.isDirectory()
+				|| (!f.getName().contains(".")));
+	}
 
-        return  (f.getName().toLowerCase().endsWith(".dcm") ||
-                 f.isDirectory()||
-                (f.getName().indexOf(".")<0));
-    }
-
-    /**
-     * The description of this filter. For example: "JPG and GIF Images"
-     *
-     * @see FileView#getName
-     */
-    public String getDescription()
-    {
-        return "DICOM Images";
-    }
+	/**
+	 * The description of this filter. For example: "JPG and GIF Images"
+	 *
+	 * @see FileView#getName
+	 */
+	public String getDescription() {
+		return "DICOM Images";
+	}
 }
