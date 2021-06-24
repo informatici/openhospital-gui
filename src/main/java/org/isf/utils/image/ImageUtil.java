d6ed65f4572916caf4bc292f6d3d6723b438b223
Andrei <AndreiDodu@users.noreply.github.com>
2021-06-24 17:26:37 +0200

    OP-438 New HTTP Client for SMS Gateway (#571)
    * bug: after changing PAT_PHOTO column type from BLOB to MEDIUMBLOB in
    PATIENT_PROFILE_PHOTO table in order to allow patient profile pictures
    in high quality, the edit patient PhotoPanel does not make a image
    preview (image size greater than the default value 160px).
    
    * refactoring of SMS sender module
    added textbelt gateway
    
    * rollback upgrade photo size
    
    * rollback image
    
    * local changes
    
    * Line: 325 -> please update TITLE with correct value
    
    * settings file rollback
    
    * code corrected for informatici
    
    Co-authored-by: Andrei Dodu <andrei.dodu@informaticisenzafrontiere.org>

--- src/main/java/org/isf/utils/image/ImageUtil.java
+++ src/main/java/org/isf/utils/image/ImageUtil.java
@@ -30,49 +30,48 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public final class ImageUtil {
-    private ImageUtil() {
-    }
 
-    public static BufferedImage scaleImage(final BufferedImage src,
-                                           final int boundWidth,
-                                           final int boundHeight){
-        final int originalWidth = src.getWidth();
-        final int originalHeight = src.getHeight();
-        int newWidth = originalWidth;
-        int newHeight = originalHeight;
+	private ImageUtil() {
+	}
 
-        // first check if we need to scale width
-        if (originalWidth > boundWidth) {
-            //scale width to fit
-            newWidth = boundWidth;
-            //scale height to maintain aspect ratio
-            newHeight = (newWidth * originalHeight) / originalWidth;
-        }
+	public static BufferedImage scaleImage(final BufferedImage src, final int boundWidth, final int boundHeight) {
+		final int originalWidth = src.getWidth();
+		final int originalHeight = src.getHeight();
+		int newWidth = originalWidth;
+		int newHeight = originalHeight;
 
-        // then check if we need to scale even with the new height
-        if (newHeight > boundHeight) {
-            //scale height to fit instead
-            newHeight = boundHeight;
-            //scale width to maintain aspect ratio
-            newWidth = (newHeight * originalWidth) / originalHeight;
-        }
+		// first check if we need to scale width
+		if (originalWidth > boundWidth) {
+			// scale width to fit
+			newWidth = boundWidth;
+			// scale height to maintain aspect ratio
+			newHeight = (newWidth * originalHeight) / originalWidth;
+		}
 
-        final BufferedImage resizedImg = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
-        final Graphics2D g2 = resizedImg.createGraphics();
-        g2.setBackground(Color.WHITE);
-        g2.clearRect(0,0,newWidth, newHeight);
-        g2.drawImage(src, 0, 0, newWidth, newHeight, null);
-        g2.dispose();
-        return resizedImg;
-    }
+		// then check if we need to scale even with the new height
+		if (newHeight > boundHeight) {
+			// scale height to fit instead
+			newHeight = boundHeight;
+			// scale width to maintain aspect ratio
+			newWidth = (newHeight * originalWidth) / originalHeight;
+		}
 
-    public static byte[] imageToByte(final BufferedImage bufferedImage) {
-        try {
-            final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-            ImageIO.write(bufferedImage, "jpg", outStream);
-            return outStream.toByteArray();
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to convert image to byte array", e);
-        }
-    }
+		final BufferedImage resizedImg = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+		final Graphics2D g2 = resizedImg.createGraphics();
+		g2.setBackground(Color.WHITE);
+		g2.clearRect(0, 0, newWidth, newHeight);
+		g2.drawImage(src, 0, 0, newWidth, newHeight, null);
+		g2.dispose();
+		return resizedImg;
+	}
+
+	public static byte[] imageToByte(final BufferedImage bufferedImage) {
+		try {
+			final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+			ImageIO.write(bufferedImage, "jpg", outStream);
+			return outStream.toByteArray();
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to convert image to byte array", e);
+		}
+	}
 }
