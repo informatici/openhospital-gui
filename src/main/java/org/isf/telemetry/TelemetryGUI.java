6019e701934410df847613711a8b545486c8d590
Andrei Dodu <andrei.dodu@hotmail.it>
2021-09-02 00:27:09 +0200

    - added headers

--- src/main/java/org/isf/telemetry/TelemetryGUI.java
+++ src/main/java/org/isf/telemetry/TelemetryGUI.java
@@ -1,3 +1,24 @@
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2021 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.isf.telemetry;
 
 import java.awt.AWTEvent;
@@ -36,7 +57,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 
-// estendere JDialog
 public class TelemetryGUI extends JDialog {
 
 	private static final long serialVersionUID = 891561833857381224L;
@@ -68,7 +88,7 @@ public class TelemetryGUI extends JDialog {
 
 		Dimension mySize = getSize();
 
-		setLocation((screensize.width - mySize.width) / 2, (screensize.height - mySize.height) / 2);
+		setLocationRelativeTo(null);
 
 		setResizable(false);
 		setVisible(true);
