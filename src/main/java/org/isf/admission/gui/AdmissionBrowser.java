1750af7850c0fe7b3ed358e0d313d3aa3ffe4cf7
Andrei Dodu <andrei.dodu@hotmail.it>
2022-06-02 22:18:22 +0200

    adapted for the core changes
    (active instead of deleted)

--- src/main/java/org/isf/admission/gui/AdmissionBrowser.java
+++ src/main/java/org/isf/admission/gui/AdmissionBrowser.java
@@ -1865,7 +1865,7 @@ public class AdmissionBrowser extends ModalJFrame {
 					admission.setType("N");
 				}
 
-				admission.setDeleted("N");
+				admission.setActive(1);
 
 				// IOoperation result
 				boolean result = false;
