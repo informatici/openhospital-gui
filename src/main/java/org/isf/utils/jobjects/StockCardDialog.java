2ca54912ee5a68b7a1a97fd07330fc9d1f2ffaf7
dbm <david@malkovsky.org>
2022-09-26 12:16:52 -0400

    OP-971 use TimeTools.truncateToSeconds() because it has a null check

--- src/main/java/org/isf/utils/jobjects/StockCardDialog.java
+++ src/main/java/org/isf/utils/jobjects/StockCardDialog.java
@@ -25,6 +25,7 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -34,7 +35,6 @@ import javax.swing.WindowConstants;
 
 import org.isf.generaldata.MessageBundle;
 import org.isf.medicals.model.Medical;
-import org.isf.utils.time.TimeTools;
 
 /**
  * @author Mwithi
@@ -160,11 +160,11 @@ public class StockCardDialog extends JDialog {
 	}
 
 	public LocalDateTime getLocalDateTimeFrom() {
-		return TimeTools.truncateToSeconds(dateFrom.getDateStartOfDay());
+		return dateFrom.getDateStartOfDay().truncatedTo(ChronoUnit.SECONDS);
 	}
 
 	public LocalDateTime getLocalDateTimeTo() {
-		return TimeTools.truncateToSeconds(dateTo.getDateStartOfDay());
+		return dateTo.getDateStartOfDay().truncatedTo(ChronoUnit.SECONDS);
 	}
 
 }
--- src/main/java/org/isf/utils/jobjects/StockLedgerDialog.java
+++ src/main/java/org/isf/utils/jobjects/StockLedgerDialog.java
@@ -25,6 +25,7 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -34,7 +35,6 @@ import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 import org.isf.generaldata.MessageBundle;
-import org.isf.utils.time.TimeTools;
 
 /**
  * @author Mwithi
@@ -116,11 +116,11 @@ public class StockLedgerDialog extends JDialog {
 	}
 
 	public LocalDateTime getLocalDateTimeFrom() {
-		return TimeTools.truncateToSeconds(dateFrom.getDateStartOfDay());
+		return dateFrom.getDateStartOfDay().truncatedTo(ChronoUnit.SECONDS);
 	}
 
 	public LocalDateTime getLocalDateTimeTo() {
-		return TimeTools.truncateToSeconds(dateTo.getDateStartOfDay());
+		return dateTo.getDateStartOfDay().truncatedTo(ChronoUnit.SECONDS);
 	}
 
 	/**
