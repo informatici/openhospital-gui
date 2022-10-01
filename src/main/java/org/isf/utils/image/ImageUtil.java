d921d943883f5c95e9393aad8ce2d8acd9924108
dbm <david@malkovsky.org>
2022-10-01 13:27:31 -0400

    OP-973 use ResampleOp() method to scale images

--- src/main/java/org/isf/utils/image/ImageUtil.java
+++ src/main/java/org/isf/utils/image/ImageUtil.java
@@ -21,48 +21,28 @@
  */
 package org.isf.utils.image;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import com.twelvemonkeys.image.ResampleOp;
+
 public final class ImageUtil {
 
 	private ImageUtil() {
 	}
 
-	public static BufferedImage scaleImage(final BufferedImage src, final int boundWidth, final int boundHeight) {
-		final int originalWidth = src.getWidth();
-		final int originalHeight = src.getHeight();
-		int newWidth = originalWidth;
-		int newHeight = originalHeight;
-
-		// first check if we need to scale width
-		if (originalWidth > boundWidth) {
-			// scale width to fit
-			newWidth = boundWidth;
-			// scale height to maintain aspect ratio
-			newHeight = (newWidth * originalHeight) / originalWidth;
+	public static BufferedImage scaleImage(BufferedImage bufferedImage, int boundWidth, int boundHeight) {
+		int originalWidth = bufferedImage.getWidth();
+		int originalHeight = bufferedImage.getHeight();
+		if (originalWidth > boundWidth || originalHeight > boundHeight) {
+			// FILTER_LANCZOS is a high quality interpolation method
+			ResampleOp resampler = new ResampleOp(boundWidth, boundWidth, ResampleOp.FILTER_LANCZOS);
+			return resampler.filter(bufferedImage, null);
 		}
-
-		// then check if we need to scale even with the new height
-		if (newHeight > boundHeight) {
-			// scale height to fit instead
-			newHeight = boundHeight;
-			// scale width to maintain aspect ratio
-			newWidth = (newHeight * originalWidth) / originalHeight;
-		}
-
-		final BufferedImage resizedImg = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
-		final Graphics2D g2 = resizedImg.createGraphics();
-		g2.setBackground(Color.WHITE);
-		g2.clearRect(0, 0, newWidth, newHeight);
-		g2.drawImage(src, 0, 0, newWidth, newHeight, null);
-		g2.dispose();
-		return resizedImg;
+		return bufferedImage;
 	}
 
 	public static byte[] imageToByte(final BufferedImage bufferedImage) {
