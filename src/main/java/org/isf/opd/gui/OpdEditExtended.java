3dc32eee38df5b10f836f0a1894652395fbed61b
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-22 19:01:02 +0100

    fix: NPE

--- src/main/java/org/isf/opd/gui/OpdEditExtended.java
+++ src/main/java/org/isf/opd/gui/OpdEditExtended.java
@@ -1685,7 +1685,7 @@ public class OpdEditExtended extends ModalJFrame implements PatientInsertExtende
 	
 	private JButton getJAnamnesisButton() {
 		if (jAnamnesisButton == null) {
-			JButton jAnamnesisButton = new JButton(MessageBundle.getMessage("angal.anamnesis.open.anamnesis.btn"));
+			jAnamnesisButton = new JButton(MessageBundle.getMessage("angal.anamnesis.open.anamnesis.btn"));
 			jAnamnesisButton.setMnemonic(MessageBundle.getMnemonic("angal.common.ok.btn.key"));
 			OpdEditExtended self = this;
 			jAnamnesisButton.addActionListener(actionEvent -> {
