59f7272e6b584bb2d1a4bfcaf5a8ada4c65e3d42
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-02 12:14:22 +0200

    [OP-1035] fix: code review points

--- src/main/java/org/isf/patient/gui/PatientInsertExtended.java
+++ src/main/java/org/isf/patient/gui/PatientInsertExtended.java
@@ -28,7 +28,6 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
