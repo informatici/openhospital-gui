5c66e8c4f638ff7ea9f6fd760940901e2ba80b46
David B Malkovsky <david@malkovsky.org>
2023-06-14 11:44:47 -0400

    Chore: Update copyright year (#1784)

--- oh.bat
+++ oh.bat
@@ -17,7 +17,7 @@ REM # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 REM # GNU General Public License for more details.
 REM #
 REM # You should have received a copy of the GNU General Public License
-REM # along with this program. If not, see <http://www.gnu.org/licenses/>.
+REM # along with this program. If not, see <https://www.gnu.org/licenses/>.
 REM #
 
 REM ################### Script configuration ###################
