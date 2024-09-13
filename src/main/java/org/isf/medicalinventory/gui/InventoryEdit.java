32e32639e94586f18ed8ca6b35d60c3d5e982061
FOFOU FONZAM Gui Arnaud <101590821+ArnaudFonzam@users.noreply.github.com>
2024-09-13 08:02:49 +0100

    Update src/main/java/org/isf/medicalinventory/gui/InventoryEdit.java
    Co-authored-by: Alessandro Domanico <alessandro.domanico@yahoo.it>

--- src/main/java/org/isf/medicalinventory/gui/InventoryEdit.java
+++ src/main/java/org/isf/medicalinventory/gui/InventoryEdit.java
@@ -936,7 +936,7 @@ public class InventoryEdit extends ModalJFrame {
 			}
 			List<MedicalInventoryRow> invRowWithoutLot = inventoryRowSearchList.stream().filter(invRow -> invRow.getLot() == null).collect(Collectors.toList());
 			if (invRowWithoutLot.size() > 0) {
-				MessageDialog.error(null, "angal.inventory.allinventoryrowshouldhavelotbeforevalidate.msg");
+				MessageDialog.error(null, "angal.inventory.allinventoryrowshouldhavelotbeforevalidation.msg");
 				return;
 			}
 			int reset = MessageDialog.yesNo(null, "angal.inventoryrow.doyoureallywanttovalidatethisinventory.msg");
