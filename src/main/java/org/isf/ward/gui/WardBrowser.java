25fb3b54719e9bd63a03b87ea022194e82701111
lnanduri2005 <likitha.nanduri@outlook.com>
2026-05-29 23:27:45 -0700

    OP-278 Apply sortable filtering to ward browser

--- src/main/java/org/isf/ward/gui/WardBrowser.java
+++ src/main/java/org/isf/ward/gui/WardBrowser.java
@@ -29,6 +29,7 @@ import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
